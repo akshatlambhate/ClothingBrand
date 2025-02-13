3e395b22bef459679172fec66bccb350d90b9c5c
akshat lambhate <akshatlambhate180302@gmail.com>
2025-02-13 20:04:33 +0530

    Home Page UI is completed and all the essentials components is mainly created

---
+++ client/src/Components/NewsLetter.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const NewsLetter = () => {
+    const onSubmitHandler =(e) =>{
+        e.preventDefault();
+        
+
+    }
+  return (
+    <div className='text-center '>
+        <p className="text-2xl font-medium text-gray-800"> Subscribe Now & get 20% off</p>
+        <p className="text-gray-400 mt-3 ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur voluptatem amet ducimus.
+        </p>
+        <form onSubmit={onSubmitHandler} action="" className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border border-gray-500 pl-3'>
+            <input className='w-full sm:flex-1 outline-none ' required type="email" id='email' placeholder='Enter Your email' />
+            <button type='submit' className='bg-black text-white text-xs px-10 py-4 '>
+                SUBSCRIBE
+
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default NewsLetter
