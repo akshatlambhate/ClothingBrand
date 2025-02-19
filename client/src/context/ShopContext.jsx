20dc9f12dfde748edc2f598c9e3863f4c151c3e7
akshat lambhate <akshatlambhate180302@gmail.com>
2025-02-19 10:37:39 +0530

    added cart functionality and linked with database

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -34,7 +34,20 @@ const ShopContextProvider = (props) => {
     }
 
     setCartItems(cartdata);
+
+    if(token){
+      try {
+        await axios.post(backendUrl + '/api/cart/add',{itemId, size},{headers:{token}})
+        
+      } catch (error) {
+         console.log(error)
+         toast.error(error.message)
+      }
+    }
   };
+
+
+
   const getCartCount = () => {
 
     let totalCount = 0;
@@ -59,6 +72,16 @@ const ShopContextProvider = (props) => {
     let cartData = structuredClone(cartItems);
     cartData[itemId][size] = quantity;
     setCartItems(cartData);
+   if(token){
+    try {
+      await axios.post(backendUrl+'/api/cart/update',{itemId,size,quantity},{headers:{token}})
+
+    } catch (error) {
+       console.log(error);
+       toast.error(error.message)
+    }
+   }
+
   };
 
   const getCartAmount = () => {
@@ -95,16 +118,34 @@ const ShopContextProvider = (props) => {
          console.log(error)
          toast.error(error)
      }
-   }
+   };
+
+    const getUserCart = async (token) => { 
+      try {
+         const response = await axios.post(backendUrl +'/api/cart/get',{},{headers:{token}});
+         if(response.data.success){
+           setCartItems(response.data.cartData);
+         }
+        
+      } catch (error) {
+        console.log(error)
+         toast.error(error)
+
+      }
+      
+     }
    useEffect(()=>{
       getProductsData();
-   },[])
+   },[]);
+
 
    useEffect(()=>{
      if(!token && localStorage.getItem('token')){
          setToken(localStorage.getItem('token'))
+        getUserCart(localStorage.getItem('token'))
      }
- },[])
+ },[]);
+
 
 
   const value = {
