a953fd985788a8c17d8c6ab2b33f2ea68d77609e
akshat lambhate <akshatlambhate180302@gmail.com>
2025-02-20 18:47:06 +0530

    try to deploy on render

--- server/server.js
+++ server/server.js
@@ -22,14 +22,17 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
+
+app.get('/',(req,res)=>{
+    res.send("API WORKING")
+})
+
+
 //Api End Points
 app.use('/api/user',userRouter);
 app.use('/api/product',productRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order',orderRouter)
 
-app.get('/',(req,res)=>{
-    res.send("API WORKING")
-})
 
 
